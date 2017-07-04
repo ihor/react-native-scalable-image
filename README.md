514bf9b26bedd0d5387acc7befcd6abad6cb5d16
spoeck <toni.spoeck@web.de>
2017-07-04 14:50:45 +0200

    feat: add props for maximum image height/width

--- README.md
+++ README.md
@@ -2,14 +2,14 @@ React Native Scalable Image
 ===========================
 React Native ```<Image/>``` component which scales width or height automatically to keep the aspect ratio. Is useful when you need to display an entire network image but don't know its size and can limit it only by width or height.
 
-The following example creates an image which fits the full screen width:
+The following example creates an image which fits the full screen width and maximum height of 300:
 
  ```jsx
 import React from 'react';
 import { Dimensions } from 'react-native';
 import Image from 'react-native-scalable-image';
 
-const image = <Image width={Dimensions.get('window').width} source={{uri: '<image uri>'}} />;
+const image = <Image width={Dimensions.get('window').width} maxHeight={300} source={{uri: '<image uri>'}} />;
  ```
 
 
@@ -22,3 +22,11 @@ Usage
 =====
 
 Specify width or height which may be calculated dynamically like in the example above. All other props are the same as in regular [React Native ```<Image/>``` component](https://facebook.github.io/react-native/docs/image.html).
+
+## props
+
+| name          | type      | default                     | description             |
+| `height`      | number    | calculated dynamically      | image height            |
+| `width`       | number    | calculated dynamically      | image width             |
+| `maxHeight`   | number    | Number.MAX_VALUE            | maximum image height    |
+| `maxWidth`    | number    | Number.MAX_VALUE            | maximum image width     |
--- index.js
+++ index.js
@@ -12,40 +12,58 @@ export default class ScalableImage extends React.Component {
     }
 
     componentDidMount() {
+
+        const  maxWidth = this.props.maxWidth?this.props.maxWidth:Number.MAX_VALUE;
+        const  maxHeight = this.props.maxHeight?this.props.maxHeight:Number.MAX_VALUE;
+
+
         Image.getSize(this.props.source.uri, (width, height) => {
             let ratio;
 
-            if (this.props.width && this.props.height) {
-                ratio = Math.min(this.props.width / width, this.props.height / height);
-            }
-            else if (this.props.width) {
-                ratio = this.props.width / width;
-            }
-            else if (this.props.height) {
-                ratio = this.props.height / height;
-            }
+        if (this.props.width && this.props.height) {
+            ratio = Math.min(this.props.width / width, this.props.height / height);
+        }
+        else if (this.props.width) {
+            ratio = this.props.width / width;
+        }
+        else if (this.props.height) {
+            ratio = this.props.height / height;
+        }
 
-            this.setState({ width: width * ratio, height: height * ratio });
-        }, console.log);
+
+        // cosider max width
+        if (width * ratio > maxWidth) {
+            ratio = (maxWidth * ratio) / (width * ratio);
+        }
+        // consider max height
+        if (height * ratio > maxHeight) {
+            ratio = (maxHeight * ratio) / (height * ratio);
+        }
+
+
+        this.setState({ width: width * ratio, height: height * ratio });
+    }, console.log);
     }
 
     render() {
         return (
             <TouchableOpacity onPress={this.props.onPress}>
-                <Image
-                    { ...this.props }
-                    style={[
-                        this.props.style,
-                        { width: this.state.width, height: this.state.height }
-                    ]}
-                />
-            </TouchableOpacity>
-        );
+    <Image
+        { ...this.props }
+        style={[
+            this.props.style,
+        { width: this.state.width, height: this.state.height }
+    ]}
+    />
+    </TouchableOpacity>
+    );
     }
 }
 
 ScalableImage.propTypes = {
     width: React.PropTypes.number,
     height: React.PropTypes.number,
+    maxWidth: React.PropTypes.number,
+    maxHeight: React.PropTypes.number,
     onPress: React.PropTypes.func,
 };
