e4d5d23d87586a10e0383d3221e8ddca401f4fa2
Ihor Burlachenko <ihor.burlachenko@gmail.com>
2017-08-17 13:04:27 +0200

    Post-merge refactoring

--- index.js
+++ index.js
@@ -12,51 +12,43 @@ export default class ScalableImage extends React.Component {
     }
 
     componentDidMount() {
-
-        const  maxWidth = this.props.maxWidth?this.props.maxWidth:Number.MAX_VALUE;
-        const  maxHeight = this.props.maxHeight?this.props.maxHeight:Number.MAX_VALUE;
-
-
         Image.getSize(this.props.source.uri, (width, height) => {
             let ratio;
 
-        if (this.props.width && this.props.height) {
-            ratio = Math.min(this.props.width / width, this.props.height / height);
-        }
-        else if (this.props.width) {
-            ratio = this.props.width / width;
-        }
-        else if (this.props.height) {
-            ratio = this.props.height / height;
-        }
+            if (this.props.width && this.props.height) {
+                ratio = Math.min(this.props.width / width, this.props.height / height);
+            }
+            else if (this.props.width) {
+                ratio = this.props.width / width;
+            }
+            else if (this.props.height) {
+                ratio = this.props.height / height;
+            }
 
+            if (width * ratio > this.props.maxWidth) {
+                ratio = this.props.maxWidth * ratio / (width * ratio);
+            }
 
-        // cosider max width
-        if (width * ratio > maxWidth) {
-            ratio = (maxWidth * ratio) / (width * ratio);
-        }
-        // consider max height
-        if (height * ratio > maxHeight) {
-            ratio = (maxHeight * ratio) / (height * ratio);
-        }
+            if (height * ratio > maxHeight) {
+                ratio = this.props.maxHeight * ratio / (height * ratio);
+            }
 
-
-        this.setState({ width: width * ratio, height: height * ratio });
-    }, console.log);
+            this.setState({ width: width * ratio, height: height * ratio });
+        }, console.log);
     }
 
     render() {
         return (
             <TouchableOpacity onPress={this.props.onPress}>
-    <Image
-        { ...this.props }
-        style={[
-            this.props.style,
-        { width: this.state.width, height: this.state.height }
-    ]}
-    />
-    </TouchableOpacity>
-    );
+                <Image
+                    { ...this.props }
+                    style={[
+                        this.props.style,
+                        { width: this.state.width, height: this.state.height }
+                    ]}
+                />
+            </TouchableOpacity>
+        );
     }
 }
 
@@ -66,4 +58,9 @@ ScalableImage.propTypes = {
     maxWidth: React.PropTypes.number,
     maxHeight: React.PropTypes.number,
     onPress: React.PropTypes.func,
+};
+
+ScalableImage.defaultProps = {
+    maxWidth: Number.MAX_VALUE,
+    maxHeight: Number.MAX_VALUE,
 };
